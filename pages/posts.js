e320098c8ccf06139cec1a86daa9b59d8e58662b
Vaibhav17031996 <vaibhavpatwal3@gmail.com>
2024-04-22 20:25:36 +0530

    done

--- pages/posts.js
+++ pages/posts.js
@@ -1,18 +1,25 @@
-import React from 'react';
-import Link from 'next/link';
+import React from "react";
+import Link from "next/link";
 
 const posts = [
-  { name: 'Post 1', path: '/posts/1' },
-  { name: 'Post 2', path: '/posts/2' },
-  { name: 'Post 3', path: '/posts/3' },
-  { name: 'Post 4', path: '/posts/4' },
+  { name: "Post 1", path: "/posts/1" },
+  { name: "Post 2", path: "/posts/2" },
+  { name: "Post 3", path: "/posts/3" },
+  { name: "Post 4", path: "/posts/4" },
 ];
 
 function Posts() {
   return (
-    <div className='posts'>
+    <div className="posts">
       <h1>Post List</h1>
-      <ul>{/* Render a list of Links from the posts object   */}</ul>
+      <ul>
+        {/* Render a list of Links from the posts object   */}
+        {posts.map((post) => (
+          <li>
+            <Link href={post.path}>{post.name}</Link>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,8 +1,12 @@
-import React from 'react';
-import { useRouter } from 'next/router';
+import React from "react";
+import { useRouter } from "next/router";
 
 function PostDetail() {
   // Complete the function
+  const router = useRouter();
+  console.log(router);
+  console.log(router.query.id);
+  return <h1 id={router.query.id}>Post Detail: {router.query.id}</h1>;
 }
 
 export default PostDetail;
